7f992445f21549e4914b96850c176696901d3dae
陽一 <yoichi.24525@gmail.com>
2026-04-14 20:50:42 +0900

    remove: delete parallel test runner script
    - Removed run_tests_parallel.sh as .cargo/config.toml provides better solution
    - All cargo test commands now automatically use 12 concurrent threads without script

--- run_tests_parallel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run cargo tests with 12 concurrent threads
-# Usage: ./run_tests_parallel.sh [--test TEST_NAME] [TEST_FILTER]
-
-export RUST_TEST_THREADS=12
-
-cargo test "$@" -- --nocapture
